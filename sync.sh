fb8f85792c36fde9414a47a699b07aa4bd6d982d
thomasyimgit <thomasyim94@gmail.com>
2017-12-15 15:08:47 +0800

    add sync script

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# chmod +x ./sync.sh
+echo '✨Synchronizing "gh-pages" with "master"...'
+git checkout gh-pages
+git pull origin master
+git push origin gh-pages
+git checkout master
+echo '✨Done.'
